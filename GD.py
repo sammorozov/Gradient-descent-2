bea354c6086bf16a8d2c1f947815933b222b8c59
sammorozov <an.sem2001@yandex.ru>
2023-02-07 18:42:10 +0300

    try2

--- GD.py
+++ GD.py
@@ -15,5 +15,26 @@ xx = 0
 lmd = 0.1
 
 x_plt = np.arange(0, 5.0, 0.1)
+f_plt = [f(x) for x in x_plt]
 
-print(x_plt)
+plt.ion()
+fig, ax = plt.subplots()
+ax.grid = True #отображение сетки
+
+ax.plot(x_plt, f_plt)
+point = ax.scatter(xx, f(xx), c = 'red')
+
+
+for i in range(N):
+    xx = xx - lmd * df(xx)
+
+    point.set_offsets([xx, f(xx)])
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(0.02)
+
+plt.ioff()
+print(xx)
+ax.scatter(xx, f(xx), c='blue')
+plt.show()
